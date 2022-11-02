9dac68aad98f30e79062acb8d061a819b938fa56
liuke <liuke19830822@qq.com>
2022-11-02 09:45:54 +0800

    修改接口配置

--- config/test.env.js
+++ config/test.env.js
@@ -4,6 +4,6 @@ const devEnv = require('./dev.env')
 
 module.exports = merge(devEnv, {
   NODE_ENV: '"testing"',
-  BASE_API: '"http://localhost:8186"',
+  BASE_API: '"http://localhost:8186/api"',
   FIGURE_BED_URL: 'http://192.168.30.100:9000'
 })
