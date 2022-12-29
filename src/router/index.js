5cae04cb49c149ef018cf47b9c374d2302306ab9
刘可 <liuke19830822@qq.com>
2022-12-29 15:15:56 +0800

    根跳转到login

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+// import HelloWorld from '@/components/HelloWorld'
 import Create from '@/components/article/create/Create'
 import Edit from '@/components/article/edit/Edit'
 import Detail from '@/components/article/detail/Detail'
